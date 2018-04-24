0ef837ee77902a53d67068be22ce0b599ba3a388
Dan.Stoian <dan@netsud.ro>
2018-04-24 09:01:25 +0300

    First commit

---
+++ app/views/partials/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/users">Manage Users</a></li>
+        <li><a href="/sendmail">Send Mail</a></li>
+    </ul>
+</nav>
